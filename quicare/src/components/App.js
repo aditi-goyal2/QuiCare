a49c05adbb0154d1662014d693c842c07a4079f7
SamriddhI-ndia <jainsamriddhi10@gmail.com>
2022-04-26 22:41:18 +0530

    home page

--- quicare/src/components/App.js
+++ quicare/src/components/App.js
@@ -1,6 +1,27 @@
+import Home from './Home';
+import Navbar from './Navbar';
+import Footer from './Footer';
+import {BrowserRouter as Router,Route,Switch,Link} from 'react-router-dom';
 const App=()=>{
     return(
-        <h1>Hello World !</h1>
+        <>
+    <Router>
+    <div className="App">
+     <center>
+        <Navbar/>
+        </center>
+      <Switch>
+        <Route exact path="/">
+          <Home/>
+        </Route>
+        <Route exact path="/about">
+          <Footer/>
+        </Route>
+      </Switch>
+    </div>
+    </Router>
+    </>
+        
     );
 }
 export default App;
---
+++ quicare/src/components/Navbar.jsx
@@ -0,0 +1,62 @@
+import { Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import './nav.css';
+const Navbar = () => {
+    
+    return (
+        <nav className="navbar navbar-expand-md navbar-light nav shadow-sm navBar">
+            <div className="container py-3">
+                <Link className="navbar-brand" to="/">
+                    <span className="fw-bold h3 d-flex align-items-center">
+                        
+                        <img className="img-fluid" src="" alt="" />
+                        <span className="fs-3 fw-bold ps-3">Quicare</span>
+                    </span>
+                </Link>
+
+                <button
+                    className="navbar-toggler"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#main-nav"
+                    aria-controls="main-nav"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                >
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+
+                <div className="collapse navbar-collapse justify-content-end align-center" id="main-nav">
+                    <ul className="navbar-nav">
+                    <li className="nav-item pe-4">
+                            <Link to="/" className="nav-link fs-5 fw-bold color">
+                            <span className="border-primary border-p-4 mr-2">Home </span>
+                            </Link>
+                        </li>
+                        <li className="nav-item pe-4">
+                            <Link to="/" className="nav-link fs-5 fw-bold color">
+                            About
+                            </Link>
+                        </li>
+                        
+                        <li className="nav-item pe-4">
+                            <Link to="/history" className="nav-link fs-5 fw-bold color">
+                               Orders
+                            </Link>
+                        </li>
+                        <li className="nav-item pe-4">
+                            <Link to="/wishlist" className="nav-link fs-5 fw-bold color">
+                                Login
+                                
+                            </Link>
+                        </li>
+                       
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
